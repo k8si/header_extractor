d37760f0ff7035326cfa9491c292fdd0481a0a91
k8si <ksilvers@cs.umass.edu>
2016-04-08 21:11:07 -0400

    update

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='header_extractor',
+      author='k8si',
+      author_email='ksilvers@cs.umass.edu',
       packages=find_packages(),
-      package_data={'': ['*.jar']})
+      package_data={'': ['*.jar']},
+      install_requires=[
+            'bibtexparser',
+            'beautifulsoup4'
+      ])
